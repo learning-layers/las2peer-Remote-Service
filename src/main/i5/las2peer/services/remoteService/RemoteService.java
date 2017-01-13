d86149643f782ab3abdc437c0a5fe8abdf4460ac
Jasper Nalbach <nalbach@dbis.rwth-aachen.de>
2017-01-13 12:24:15 +0100

    added console output in request

--- src/main/i5/las2peer/services/remoteService/RemoteService.java
+++ src/main/i5/las2peer/services/remoteService/RemoteService.java
@@ -9,6 +9,7 @@ public class RemoteService extends Service {
 	}
 
 	public String sayHello() {
+		System.out.println("\033[31m Received request, sending response... \033[0m");
 		return "I am a service running at RWTH Aachen!";
 	}
 
